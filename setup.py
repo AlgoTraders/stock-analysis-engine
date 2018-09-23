cd559d249e250d1d90186033c76d32599f2e0898
jay-johnson <jay.p.h.johnson@gmail.com>
2018-09-23 07:39:25 +0000

    version bump for docker container

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ sys.path.insert(
 setup(
     name='stock-analysis-engine',
     cmdclass={'build_py': build_py},
-    version='1.0.4',
+    version='1.0.5',
     description=(
         'Stock Analysis Engine - '
         'Use this to get pricing data for tickers '
