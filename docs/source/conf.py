82db5882a8f6548bec8a58228a4bd5685de7a3be
jay-johnson <jay.p.h.johnson@gmail.com>
2018-09-26 05:36:05 +0000

    trying sphinx celery source code parsing again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ import sys
 from unittest.mock import MagicMock
 from recommonmark.parser import CommonMarkParser
 
+sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath('../analysis_engine/scripts'))
 sys.path.insert(0, os.path.abspath('../analysis_engine/work_tasks'))
@@ -78,14 +79,14 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = (
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'celery.contrib.sphinx',
-]
+)
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
