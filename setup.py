8543c931ae2a0ab17534910bb2a6b21152843693
jay-johnson <jay.p.h.johnson@gmail.com>
2018-10-20 18:21:20 +0000

    new fetch and extract notebook stub and error-level logging for reducing the output in jupyter

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ sys.path.insert(
 setup(
     name='stock-analysis-engine',
     cmdclass={'build_py': build_py},
-    version='1.1.16',
+    version='1.1.17',
     description=(
         'Stock Analysis Engine - '
         'Use this to get pricing data for tickers '
