1f36752a407dd00d54fb48cde88013a0cb8d15e0
jay-johnson <jay.p.h.johnson@gmail.com>
2018-11-10 08:42:17 +0000

    tool for extracting datasets and storing the pandas.DataFrames as pretty-printed json files in /tmp. allows for developing algorithms offline. might add a couple to the unittests for indicator testing with ta-lib

---
+++ tools/extract_to_file.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+"""
+Tool for extracting datasets from
+redis for developing and tuning algorithms offline
+
+.. note:: This tool requires redis to be running with
+    fetched datasets already stored in supported
+    keys
+"""
+
+import os
+import json
+from analysis_engine.consts import ppj
+from analysis_engine.extract import extract
+
+ticker = 'SPY'
+res = extract(
+    ticker=ticker)
+
+daily_df = res['SPY']['daily']
+minute_df = res['SPY']['minute']
+
+daily_file = '/tmp/{}-daily.json'.format(
+    ticker.lower())
+minute_file = '/tmp/{}-minute.json'.format(
+    ticker.lower())
+print('converting dates')
+
+print(
+    'converting to pretty printed json file={}'.format(
+        daily_file))
+daily_out_json = ppj(json.loads(daily_df.iloc[-100:-1].to_json(
+    orient='records',
+    date_format='iso')))
+print(
+    'writing to file daily_file={}'.format(
+        daily_file))
+with open(daily_file, 'w') as f:
+    f.write(daily_out_json)
+
+if not os.path.exists(daily_file):
+    print('failed creating daily ticker={} daily_file={}'.format(
+        ticker,
+        daily_file))
+
+minute_out_json = ppj(json.loads(minute_df.iloc[-100:-1].to_json(
+    orient='records',
+    date_format='iso')))
+print(
+    'writing to file minute_file={}'.format(
+        minute_file))
+with open(minute_file, 'w') as f:
+    f.write(minute_out_json)
+
+if not os.path.exists(minute_file):
+    print('failed creating minute ticker={} minute_file={}'.format(
+        ticker,
+        minute_file))
