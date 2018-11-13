1593a2aca4b3264b683723f9329560c57840a691
jay-johnson <jay.p.h.johnson@gmail.com>
2018-11-13 08:46:48 +0000

    initial run algorithm support with: sa.py -t SPY -g /opt/sa/analysis_engine/mocks/example_algo_minute.py

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ sys.path.insert(
 setup(
     name='stock-analysis-engine',
     cmdclass={'build_py': build_py},
-    version='1.1.55',
+    version='1.2.0',
     description=(
         'Stock Analysis Engine - '
         'Use this to get pricing data for tickers '
