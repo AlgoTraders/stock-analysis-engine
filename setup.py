bbb816c92f6cdf65bbe134750f6eec308731d0d6
Jay <jay-johnson@users.noreply.github.com>
2018-11-30 10:31:12 -0800

    Adding boto3 to the pips after getting readthedocs to build the sphinx docs once celery-connectors was removed (due to pycurl pip) (#237)
    * read the docs works so making a new build
    
    * travis failed with missing boto3 after removing celery-connectors

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ sys.path.insert(
 setup(
     name='stock-analysis-engine',
     cmdclass={'build_py': build_py},
-    version='1.4.1',
+    version='1.4.2',
     description=(
         'Build and tune your own investment '
         'algorithms using a distributed, scalable '
