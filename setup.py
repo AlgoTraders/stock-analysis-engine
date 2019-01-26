39fa39fa4c3e55b25e6ef2fe729431f9c171792c
jay-johnson <jay.p.h.johnson@gmail.com>
2019-01-26 06:18:09 +0000

    new minor version bump

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ sys.path.insert(
 setup(
     name='stock-analysis-engine',
     cmdclass={'build_py': build_py},
-    version='1.5.10',
+    version='1.6.0',
     description=(
         'Backtest 1000s of minute-by-minute '
         'trading algorithms with automated '
