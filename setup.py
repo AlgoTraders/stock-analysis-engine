4f3fee797354426c636e5dff89aec6a0d652ebf6
jay-johnson <jay.p.h.johnson@gmail.com>
2019-02-04 17:35:48 +0000

    adding buy and sell stats to the trading history for use with predictions

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ sys.path.insert(
 setup(
     name='stock-analysis-engine',
     cmdclass={'build_py': build_py},
-    version='1.6.15',
+    version='1.6.16',
     description=(
         'Backtest 1000s of minute-by-minute '
         'trading algorithms. Automated '
