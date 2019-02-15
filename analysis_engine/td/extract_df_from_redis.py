02b7c73a39f40e1907f1f4cf83c82c72ed2a273e
jay-johnson <jay.p.h.johnson@gmail.com>
2019-02-15 06:20:59 +0000

    proposed option fetch that is aware of the latest minute intraday pricing so that the in-the-money, at-the-money and out-of-the-money options are limited to [current close - 10 strikes, atm the money strike, current close + 10 strikes]. This helps to reduce the amount of data being stored in redis for intraday option chains.

--- analysis_engine/td/extract_df_from_redis.py
+++ analysis_engine/td/extract_df_from_redis.py
@@ -44,7 +44,176 @@ def extract_option_calls_dataset(
     :param work_dict: dictionary of args
     :param scrub_mode: type of scrubbing handler to run
     """
-    return extract_option_dataset('calls', work_dict, scrub_mode=scrub_mode)
+    label = f'{work_dict.get("label", "extract")}'
+    ds_id = work_dict.get('ticker')
+    df_type = td_consts.DATAFEED_TD_CALLS
+    df_str = td_consts.get_datafeed_str_td(df_type=df_type)
+    redis_key = work_dict.get(
+        'redis_key',
+        work_dict.get('tdcalls', 'missing-redis-key'))
+    s3_key = work_dict.get(
+        's3_key',
+        work_dict.get('tdcalls', 'missing-s3-key'))
+    redis_host = work_dict.get(
+        'redis_host',
+        None)
+    redis_port = work_dict.get(
+        'redis_port',
+        None)
+    redis_db = work_dict.get(
+        'redis_db',
+        ae_consts.REDIS_DB)
+    verbose = work_dict.get(
+        'verbose_td',
+        False)
+
+    if verbose:
+        log.info(
+            f'{label} - {df_str} - start - redis_key={redis_key} '
+            f's3_key={s3_key}')
+
+    if not redis_host and not redis_port:
+        redis_host = ae_consts.REDIS_ADDRESS.split(':')[0]
+        redis_port = ae_consts.REDIS_ADDRESS.split(':')[1]
+
+    exp_date_str = None
+    calls_df = None
+    status = ae_consts.NOT_RUN
+    try:
+        redis_rec = redis_get.get_data_from_redis_key(
+            label=label,
+            host=redis_host,
+            port=redis_port,
+            db=redis_db,
+            password=work_dict.get('password', None),
+            key=redis_key,
+            decompress_df=True)
+
+        status = redis_rec['status']
+        if verbose:
+            log.info(
+                f'{label} - {df_str} redis get data key={redis_key} '
+                f'status={ae_consts.get_status(status=status)}')
+
+        if status == ae_consts.SUCCESS:
+            calls_json = None
+            if 'calls' in redis_rec['rec']['data']:
+                calls_json = redis_rec['rec']['data']['calls']
+            else:
+                calls_json = redis_rec['rec']['data']
+            if verbose:
+                log.info(f'{label} - {df_str} redis convert calls to df')
+            exp_date_str = None
+            try:
+                calls_df = pd.read_json(
+                    calls_json,
+                    orient='records')
+                if len(calls_df.index) == 0:
+                    return ae_consts.SUCCESS, None
+                if 'date' not in calls_df:
+                    log.debug(
+                        'failed to find date column in TD calls '
+                        f'df={calls_df} from lens={len(calls_df.index)}')
+                    return ae_consts.SUCCESS, None
+                calls_df.sort_values(
+                        by=[
+                            'date',
+                            'strike'
+                        ])
+                """
+                for i, r in calls_df.iterrows():
+                    print(r['date'])
+                convert_epochs = [
+                    'ask_date',
+                    'bid_date',
+                    'trade_date'
+                ]
+                for c in convert_epochs:
+                    if c in calls_df:
+                        calls_df[c] = pd.DatetimeIndex(pd.to_datetime(
+                            calls_df[c],
+                            format=ae_consts.COMMON_TICK_DATE_FORMAT
+                        )).tz_localize(
+                            'UTC').tz_convert(
+                                'US/Eastern')
+                # dates converted
+                """
+                exp_date_str = (
+                    calls_df['exp_date'].iloc[-1])
+
+                calls_df['date'] = calls_df['date'].dt.strftime(
+                    ae_consts.COMMON_TICK_DATE_FORMAT)
+
+            except Exception as f:
+                not_fixed = True
+                if (
+                        f'Can only use .dt accessor with '
+                        f'datetimelike values') in str(f):
+                    try:
+                        log.critical(
+                            f'fixing dates in {redis_key}')
+                        # remove epoch second data and
+                        # use only the millisecond date values
+                        calls_df['date'][calls_df['date'] < 1600000] = None
+                        calls_df = calls_df.dropna(axis=0, how='any')
+                        fmt = ae_consts.COMMON_TICK_DATE_FORMAT
+                        calls_df['date'] = pd.to_datetime(
+                            calls_df['date'],
+                            unit='ms').dt.strftime(fmt)
+                        not_fixed = False
+                    except Exception as g:
+                        log.critical(
+                            f'failed to parse date column {calls_df["date"]} '
+                            f'with dt.strftime ex={f} and EPOCH EX={g}')
+                        return ae_consts.EMPTY, None
+                # if able to fix error or not
+
+                if not_fixed:
+                    log.error(
+                        f'{label} - {df_str} redis_key={redis_key} '
+                        f'no calls df found or ex={f}')
+                    return ae_consts.EMPTY, None
+                # if unable to fix - return out
+
+                log.error(
+                    f'{label} - {df_str} redis_key={redis_key} '
+                    f'no calls df found or ex={f}')
+                return ae_consts.EMPTY, None
+            # end of try/ex to convert to df
+            if verbose:
+                log.info(
+                    f'{label} - {df_str} redis_key={redis_key} '
+                    f'calls={len(calls_df.index)} exp_date={exp_date_str}')
+        else:
+            if verbose:
+                log.info(
+                    f'{label} - {df_str} did not find valid redis '
+                    f'option calls in redis_key={redis_key} '
+                    f'status={ae_consts.get_status(status=status)}')
+
+    except Exception as e:
+        log.debug(
+            f'{label} - {df_str} - ds_id={ds_id} failed getting option '
+            f'calls from redis={redis_host}:{redis_port}@{redis_db} '
+            f'key={redis_key} ex={e}')
+        return ae_consts.ERR, None
+    # end of try/ex extract from redis
+
+    if verbose:
+        log.info(
+            f'{label} - {df_str} ds_id={ds_id} extract scrub={scrub_mode}')
+
+    scrubbed_df = scrub_utils.extract_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=df_type,
+        msg_format='df={} date_str={}',
+        ds_id=ds_id,
+        df=calls_df)
+
+    status = ae_consts.SUCCESS
+
+    return status, scrubbed_df
 # end of extract_option_calls_dataset
 
 
@@ -59,31 +228,9 @@ def extract_option_puts_dataset(
     :param work_dict: dictionary of args
     :param scrub_mode: type of scrubbing handler to run
     """
-    return extract_option_dataset('puts', work_dict, scrub_mode=scrub_mode)
-# end of extract_option_puts_dataset
-
-
-def extract_option_dataset(
-        option,
-        work_dict,
-        scrub_mode='sort-by-date'):
-    """extract_option_dataset
-
-    Extract the TD options puts/calls for a ticker and
-    return it as a ``pandas.Dataframe``
-
-    :param option: either 'calls' or 'puts' option type
-    :param work_dict: dictionary of args
-    :param scrub_mode: type of scrubbing handler to run
-    """
-    if not option or option not in ['calls', 'puts']:
-        return ae_consts.NOT_RUN, None
-
     label = f'{work_dict.get("label", "extract")}'
     ds_id = work_dict.get('ticker')
-    df_type = (
-        td_consts.DATAFEED_TD_PUTS if option == 'puts'
-        else td_consts.DATAFEED_TD_CALLS)
+    df_type = td_consts.DATAFEED_TD_PUTS
     df_str = td_consts.get_datafeed_str_td(df_type=df_type)
     redis_key = work_dict.get(
         'redis_key',
@@ -114,7 +261,7 @@ def extract_option_dataset(
         redis_port = ae_consts.REDIS_ADDRESS.split(':')[1]
 
     exp_date_str = None
-    options_df = None
+    puts_df = None
     status = ae_consts.NOT_RUN
     try:
         redis_rec = redis_get.get_data_from_redis_key(
@@ -133,25 +280,25 @@ def extract_option_dataset(
                 f'status={ae_consts.get_status(status=status)}')
 
         if status == ae_consts.SUCCESS:
-            options_json = None
-            if f'{option}' in redis_rec['rec']['data']:
-                options_json = redis_rec['rec']['data'][f'{option}']
+            puts_json = None
+            if 'puts' in redis_rec['rec']['data']:
+                puts_json = redis_rec['rec']['data']['puts']
             else:
-                options_json = redis_rec['rec']['data']
+                puts_json = redis_rec['rec']['data']
             if verbose:
-                log.info(f'{label} - {df_str} redis convert {option} to df')
+                log.info(f'{label} - {df_str} redis convert puts to df')
             try:
-                options_df = pd.read_json(
-                    options_json,
+                puts_df = pd.read_json(
+                    puts_json,
                     orient='records')
-                if len(options_df.index) == 0:
+                if len(puts_df.index) == 0:
                     return ae_consts.SUCCESS, None
-                if 'date' not in options_df:
+                if 'date' not in puts_df:
                     log.debug(
-                        f'failed to find date column in TD {option} '
-                        f'df={options_df} len={len(options_df.index)}')
+                        'failed to find date column in TD puts '
+                        f'df={puts_df} len={len(puts_df.index)}')
                     return ae_consts.SUCCESS, None
-                options_df.sort_values(
+                puts_df.sort_values(
                         by=[
                             'date',
                             'strike'
@@ -165,9 +312,9 @@ def extract_option_dataset(
                     'trade_date'
                 ]
                 for c in convert_epochs:
-                    if c in options_df:
-                        options_df[c] = pd.DatetimeIndex(pd.to_datetime(
-                            options_df[c],
+                    if c in puts_df:
+                        puts_df[c] = pd.DatetimeIndex(pd.to_datetime(
+                            puts_df[c],
                             format=ae_consts.COMMON_TICK_DATE_FORMAT
                         )).tz_localize(
                             'UTC').tz_convert(
@@ -175,32 +322,32 @@ def extract_option_dataset(
                 # dates converted
                 """
                 exp_date_str = (
-                    options_df['exp_date'].iloc[-1])
+                    puts_df['exp_date'].iloc[-1])
 
-                options_df['date'] = options_df['date'].dt.strftime(
+                puts_df['date'] = puts_df['date'].dt.strftime(
                     ae_consts.COMMON_TICK_DATE_FORMAT)
 
             except Exception:
                 log.debug(
                     f'{label} - {df_str} redis_key={redis_key} '
-                    f'no {option} df found')
+                    'no puts df found')
                 return ae_consts.EMPTY, None
             # end of try/ex to convert to df
             if verbose:
                 log.info(
-                    f'{label} - {df_str} redis_key={redis_key} {option}='
-                    f'{len(options_df.index)} exp_date={exp_date_str}')
+                    f'{label} - {df_str} redis_key={redis_key} '
+                    f'puts={len(puts_df.index)} exp_date={exp_date_str}')
         else:
             if verbose:
                 log.info(
                     f'{label} - {df_str} did not find valid redis '
-                    f'option {option} in redis_key={redis_key} '
+                    f'option puts in redis_key={redis_key} '
                     f'status={ae_consts.get_status(status=status)}')
 
     except Exception as e:
         log.debug(
             f'{label} - {df_str} - ds_id={ds_id} failed getting option '
-            f'{option} from redis={redis_host}:{redis_port}@{redis_db} '
+            f'puts from redis={redis_host}:{redis_port}@{redis_db} '
             f'key={redis_key} ex={e}')
         return ae_consts.ERR, None
     # end of try/ex extract from redis
@@ -215,9 +362,9 @@ def extract_option_dataset(
         datafeed_type=df_type,
         msg_format='df={} date_str={}',
         ds_id=ds_id,
-        df=options_df)
+        df=puts_df)
 
     status = ae_consts.SUCCESS
 
     return status, scrubbed_df
-# end of extract_option_dataset
+# end of extract_option_puts_dataset
--- analysis_engine/td/fetch_api.py
+++ analysis_engine/td/fetch_api.py
@@ -30,49 +30,13 @@ def fetch_calls(
         scrub_mode='sort-by-date'):
     """fetch_calls
 
-    Fetch the Tradier calls data for a ticker and
+    Fetch Tradier option calls for a ticker and
     return it as a ``pandas.DataFrame``.
 
     :param work_dict: dictionary of args
     :param scrub_mode: type of scrubbing handler to run
     """
-    return fetch('call', work_dict, scrub_mode=scrub_mode)
-# end of fetch_calls
-
-
-def fetch_puts(
-        work_dict,
-        scrub_mode='sort-by-date'):
-    """fetch_puts
-
-    Fetch the Tradier puts intraday data for a ticker and
-    return it as a ``pandas.DataFrame``.
-
-    :param work_dict: dictionary of args
-    :param scrub_mode: type of scrubbing handler to run
-    """
-    return fetch('put', work_dict, scrub_mode=scrub_mode)
-# end of fetch_puts
-
-
-def fetch(
-        fetch_type,
-        work_dict,
-        scrub_mode='sort-by-date'):
-    """fetch
-
-    Fetch the Tradier calls/puts data for a ticker and
-    return it as a ``pandas.DataFrame``.
-
-    :param fetch_type: fetch type of either 'call' or 'put'
-    :param work_dict: dictionary of args
-    :param scrub_mode: type of scrubbing handler to run
-    """
-    if not fetch_type or fetch_type not in ['call', 'put']:
-        return ae_consts.NOT_RUN
-    is_put = bool(fetch_type == 'put')
-    datafeed_type = (
-        td_consts.DATAFEED_TD_PUTS if is_put else td_consts.DATAFEED_TD_CALLS)
+    datafeed_type = td_consts.DATAFEED_TD_CALLS
     ticker = work_dict.get(
         'ticker',
         None)
@@ -82,10 +46,16 @@ def fetch(
     exp_date = work_dict.get(
         'exp_date',
         None)
+    latest_pricing = work_dict.get(
+        'latest_pricing',
+        {})
+    latest_close = latest_pricing.get(
+        'close',
+        None)
 
     log.debug(
-        f'{label} - {fetch_type} - scrub_mode={scrub_mode} '
-        f'args={work_dict} ticker={ticker}')
+        f'{label} - calls - close={latest_close} '
+        f'ticker={ticker}')
 
     exp_date = opt_dates.option_expiration().strftime(
         ae_consts.COMMON_DATE_FORMAT)
@@ -102,12 +72,12 @@ def fetch(
     if res.status_code != requests.codes.OK:
         if res.status_code in [401, 403]:
             log.critical(
-                'Please check the TD_TOKEN is correct '
+                f'Please check the TD_TOKEN is correct '
                 f'received {res.status_code} during '
-                f'fetch for: {fetch_type}s')
+                f'fetch for: calls')
         else:
             log.info(
-                f'failed to get {fetch_type} with response={res} '
+                f'failed to get call with response={res} '
                 f'code={res.status_code} '
                 f'text={res.text}')
         return ae_consts.EMPTY, pd.DataFrame([{}])
@@ -118,8 +88,9 @@ def fetch(
 
     if len(org_records) == 0:
         log.info(
-            f'failed to get {fetch_type} records '
-            f'text={res.text}')
+            'failed to get call records '
+            'text={}'.format(
+                res.text))
         return ae_consts.EMPTY, pd.DataFrame([{}])
 
     options_list = []
@@ -140,10 +111,10 @@ def fetch(
         node['created'] = created_minute
         node['ticker'] = ticker
         if (
-                node['option_type'] == fetch_type and
-                node['expiration_type'] == 'standard'):
-            node['opt_type'] = int(
-                ae_consts.OPTION_PUT if is_put else ae_consts.OPTION_CALL)
+                node['option_type'] == 'call' and
+                node['expiration_type'] == 'standard' and
+                float(node['bid']) > 0.01):
+            node['opt_type'] = int(ae_consts.OPTION_CALL)
             node['exp_date'] = node['expiration_date']
 
             new_node = {}
@@ -184,21 +155,36 @@ def fetch(
             'strike'
         ],
         ascending=True)
-    num_chains = len(full_df.index)
-    mid_chain_idx = int(num_chains / 2)
-    low_idx = int(
-        mid_chain_idx - (30 if is_put else 20))
-    high_idx = int(
-        mid_chain_idx + (20 if is_put else 30))
-    if low_idx < 0:
-        low_idx = 0
-    if high_idx > num_chains:
-        high_idx = num_chains
 
-    df = full_df[low_idx:high_idx].copy().sort_values(by=[
-            'date',
-            'strike'
-        ]).reset_index()
+    num_chains = len(full_df.index)
+
+    df = None
+    if latest_close:
+        df_filter = (
+            (full_df['strike'] >=
+                (latest_close - 10.0)) &
+            (full_df['strike'] <=
+                (latest_close + 10.0)))
+        df = full_df[df_filter].copy().sort_values(
+            by=[
+                'date',
+                'strike'
+            ]).reset_index()
+    else:
+        mid_chain_idx = int(num_chains / 2)
+        low_idx = int(
+            mid_chain_idx - 200)
+        high_idx = int(
+            mid_chain_idx + 200)
+        if low_idx < 0:
+            low_idx = 0
+        if high_idx > num_chains:
+            high_idx = num_chains
+        df = full_df[low_idx:high_idx].copy().sort_values(
+            by=[
+                'date',
+                'strike'
+            ]).reset_index()
 
     scrubbed_df = scrub_utils.ingress_scrub_dataset(
         label=label,
@@ -210,4 +196,178 @@ def fetch(
         df=df)
 
     return ae_consts.SUCCESS, scrubbed_df
-# end of fetch
+# end of fetch_calls
+
+
+def fetch_puts(
+        work_dict,
+        scrub_mode='sort-by-date'):
+    """fetch_puts
+
+    Fetch Tradier option puts for a ticker and
+    return it as a ``pandas.DataFrame``.
+
+    :param work_dict: dictionary of args
+    :param scrub_mode: type of scrubbing handler to run
+    """
+    datafeed_type = td_consts.DATAFEED_TD_PUTS
+    ticker = work_dict.get(
+        'ticker',
+        None)
+    label = work_dict.get(
+        'label',
+        None)
+    exp_date = work_dict.get(
+        'exp_date',
+        None)
+    latest_pricing = work_dict.get(
+        'latest_pricing',
+        {})
+    latest_close = latest_pricing.get(
+        'close',
+        None)
+
+    log.debug(
+        f'{label} - puts - close={latest_close} '
+        f'ticker={ticker}')
+
+    exp_date = opt_dates.option_expiration().strftime(
+        ae_consts.COMMON_DATE_FORMAT)
+    use_url = td_consts.TD_URLS['options'].format(
+        ticker,
+        exp_date)
+    headers = td_consts.get_auth_headers()
+    session = requests.Session()
+    session.headers = headers
+    res = url_helper.url_helper(sess=session).get(
+        use_url
+    )
+
+    if res.status_code != requests.codes.OK:
+        if res.status_code in [401, 403]:
+            log.critical(
+                f'Please check the TD_TOKEN is correct '
+                f'received {res.status_code} during '
+                f'fetch for: puts')
+        else:
+            log.info(
+                f'failed to get put with response={res} '
+                f'code={res.status_code} '
+                f'text={res.text}')
+        return ae_consts.EMPTY, pd.DataFrame([{}])
+    records = json.loads(res.text)
+    org_records = records.get(
+        'options', {}).get(
+            'option', [])
+
+    if len(org_records) == 0:
+        log.info(
+            'failed to get put records '
+            'text={}'.format(
+                res.text))
+        return ae_consts.EMPTY, pd.DataFrame([{}])
+
+    options_list = []
+
+    # assumes UTC conversion will work with the system clock
+    created_minute = (
+        datetime.datetime.utcnow() - datetime.timedelta(hours=5)).strftime(
+            '%Y-%m-%d %H:%M:00')
+    last_close_date = ae_utils.get_last_close_str(
+        fmt='%Y-%m-%d %H:%M:00')
+
+    # hit bug where dates were None
+    if not last_close_date:
+        last_close_date = created_minute
+
+    for node in org_records:
+        node['date'] = last_close_date
+        node['created'] = created_minute
+        node['ticker'] = ticker
+        if (
+                node['option_type'] == 'put' and
+                node['expiration_type'] == 'standard' and
+                float(node['bid']) > 0.01):
+            node['opt_type'] = int(ae_consts.OPTION_PUT)
+            node['exp_date'] = node['expiration_date']
+
+            new_node = {}
+            for col in td_consts.TD_OPTION_COLUMNS:
+                if col in node:
+                    if col in td_consts.TD_EPOCH_COLUMNS:
+                        # trade_date can be None
+                        if node[col] == 0:
+                            new_node[col] = None
+                        else:
+                            new_node[col] = ae_utils.epoch_to_dt(
+                                epoch=node[col]/1000,
+                                use_utc=False,
+                                convert_to_est=True).strftime(
+                                    ae_consts.COMMON_TICK_DATE_FORMAT)
+                            """
+                            Debug epoch ms converter:
+                            """
+                            """
+                            print('-----------')
+                            print(col)
+                            print(node[col])
+                            print(new_node[col])
+                            print('===========')
+                            """
+                        # if/else valid date
+                    else:
+                        new_node[col] = node[col]
+                    # if date column to convert
+                # if column is in the row
+            # convert all columns
+
+            options_list.append(new_node)
+    # end of records
+
+    full_df = pd.DataFrame(options_list).sort_values(
+        by=[
+            'strike'
+        ],
+        ascending=True)
+
+    num_chains = len(full_df.index)
+
+    df = None
+    if latest_close:
+        df_filter = (
+            (full_df['strike'] >=
+                (latest_close - 10.0)) &
+            (full_df['strike'] <=
+                (latest_close + 10.0)))
+        df = full_df[df_filter].copy().sort_values(
+            by=[
+                'date',
+                'strike'
+            ]).reset_index()
+    else:
+        mid_chain_idx = int(num_chains / 2)
+        low_idx = int(
+            mid_chain_idx - 200)
+        high_idx = int(
+            mid_chain_idx + 200)
+        if low_idx < 0:
+            low_idx = 0
+        if high_idx > num_chains:
+            high_idx = num_chains
+        df = full_df[low_idx:high_idx].copy().sort_values(
+            by=[
+                'date',
+                'strike'
+            ]).reset_index()
+
+    scrubbed_df = scrub_utils.ingress_scrub_dataset(
+        label=label,
+        scrub_mode=scrub_mode,
+        datafeed_type=datafeed_type,
+        msg_format='df={} date_str={}',
+        ds_id=ticker,
+        date_str=exp_date,
+        df=df)
+
+    return ae_consts.SUCCESS, scrubbed_df
+# end of fetch_puts
